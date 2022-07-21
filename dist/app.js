44419c5d19660a646b9ed5ed9c5327b08483f972
mathieuDC <mdecampou@gmail.com>
2022-07-21 21:43:51 +0200

    Adding groups router

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var groupsRouter = require('./routes/groups');
 var app = express();
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/groups', groupsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));

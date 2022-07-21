a319e72d783bb376ce8c188b9226fef0fae1a2d7
mathieuDC <mdecampou@gmail.com>
2022-07-21 21:46:01 +0200

    Using model in router.groups

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -1,10 +1,38 @@
 import express, { Request, Response } from "express";
+import CGroup from "../models/Group";
+import User from "../models/User";
 
 var router = express.Router();
 
+function groupTest ()  {
+  const group = new CGroup("OM");
+
+  const user1 = new User(1, "Guendouzi", "Mateo");
+  const user2 = new User(2, "Da Silva", "Gerson");
+  const user3 = new User(3, "Payet", "Dimitry");
+
+  group.add(user1);
+  group.add(user2);
+  group.add(user3);
+
+  group.addTransaction({id: 1, name: "Arbitre", giver: user1, receiver: user2, amount: 10});
+  group.addTransaction({id: 2, name: "Courses", giver: user2, receiver: user3, amount: 27});
+  group.addTransaction({id: 3, name: "Essences", giver: user2, receiver: user1, amount: 3});
+  group.addTransaction({id: 4, name: "Resto", giver: user1, receiver: user2, amount: 12});
+  group.addTransaction({id: 5, name: "Bar", giver: user3, receiver: user2, amount: 24});
+  group.addTransaction({id: 6, name: "Bieres", giver: user2, receiver: user1, amount: 150});
+  group.addTransaction({id: 7, name: "Course", giver: user1, receiver: user3, amount: 60});
+
+  group.computeDebt();
+
+  return group;
+};
+
 /* GET groups listing. */
 router.get('/', function(req: Request, res: Response) {
-  res.send('respond with a group');
+  const group = groupTest();
+  res.type('json');
+  res.send(JSON.stringify(group));
 });
 
 module.exports = router;

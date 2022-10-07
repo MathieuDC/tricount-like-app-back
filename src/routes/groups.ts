ea7d74a1feb34fdc153e9f27be29eeae9d5c8af2
mathieuDC <mdecampou@gmail.com>
2022-10-07 09:35:44 +0200

    Adding group controller in router

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import CGroup from "../models/Group";
 import User from "../models/User";
 
+const controller = require('./../controllers/groups')
+
 var router = express.Router();
 
 function groupTest ()  {
@@ -33,6 +35,25 @@ router.get('/', function(req: Request, res: Response) {
   const group = groupTest();
   res.type('json');
   res.send(JSON.stringify(group));
+});
+
+/* POST a new group. */
+router.post('/', function(req: Request, res: Response) {
+  const group = req.body;
+  controller.post(group).then((newGroup: CGroup) => {
+    res.type('json');
+    res.json(newGroup);
+  })
+});
+
+/* POST a new user in a specific group. */
+router.post('/:id', function(req: Request, res: Response) {
+  const groupId = req.params.id; 
+  const user = req.body;
+  controller.postUser(groupId, user).then((group: CGroup) => {
+    res.type('json');
+    res.json(group);
+  })
 });
 
 module.exports = router;
